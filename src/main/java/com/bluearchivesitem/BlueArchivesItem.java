3f94b135c330bf01ed468813daeab569832bd951
Mikaqwa <3059630032@qq.com>
2026-02-25 14:33:24 +0800

    修复末地无法生成矿石(开发者的脑子抽了,忘记注册了awa)

--- src/main/java/com/bluearchivesitem/BlueArchivesItem.java
+++ src/main/java/com/bluearchivesitem/BlueArchivesItem.java
@@ -1,7 +1,11 @@
 package com.bluearchivesitem;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.bluearchivesitem.item.ModItemGroups;
 import com.bluearchivesitem.item.ModItems;
+
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
@@ -11,14 +15,11 @@ import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.feature.PlacedFeature;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class BlueArchivesItem implements ModInitializer {
 	public static final String MOD_ID = "blue_archives_item";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static final RegistryKey<PlacedFeature> CUSTOM_ORE_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier("blue_archives_item","ore_custom"));//注册一个PlacedFeature的RegistryKey，命名空间为tutorial，路径为ore_custom。后续在生成类中使用这个RegistryKey来添加自定义矿石生成。
-
+	public static final RegistryKey<PlacedFeature> CUSTOM_ORE_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier("blue_archives_item","ore_custom"));//注册一个PlacedFeature的RegistryKey，路径为ore_custom。后续在生成类中使用这个RegistryKey来添加自定义矿石生成。
+	public static final RegistryKey<PlacedFeature> END_CUSTOM_ORE_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier("blue_archives_item","ore_end_custom"));//注册一个PlacedFeature的RegistryKey，路径为ore_end_custom。后续在生成类中使用这个RegistryKey来添加自定义矿石生成。
 
 	@Override
 	public void onInitialize() {
@@ -26,7 +27,7 @@ public class BlueArchivesItem implements ModInitializer {
 		com.bluearchivesitem.block.ModBlocks.registerBlocks(); // 注册方块
 		ModItemGroups.registerItemGroups(); // 注册物品栏
 		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, CUSTOM_ORE_PLACED_KEY);// 在主世界生成青辉石矿石
-		BiomeModifications.addFeature(BiomeSelectors.foundInTheEnd(), GenerationStep.Feature.UNDERGROUND_ORES, CUSTOM_ORE_PLACED_KEY);//在末地生成
+		BiomeModifications.addFeature(BiomeSelectors.foundInTheEnd(), GenerationStep.Feature.RAW_GENERATION, END_CUSTOM_ORE_PLACED_KEY);//在末地生成
 		LOGGER.info("Blue Archive's Item Mod loaded!");
 	}
 }
